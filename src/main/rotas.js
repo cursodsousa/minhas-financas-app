dc7d27808b954d7088a0eca64d1623aa5cd0c0d4
Dougllas Sousa <dougllasfps@gmail.com>
2019-10-27 10:29:04 -0300

    implementando rotas autenticadas

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -6,17 +6,38 @@ import CadastroUsuario from '../views/cadastroUsuario'
 import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos'
 import CadastroLancamentos from '../views/lancamentos/cadastro-lancamentos'
 
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom'
+
+const isUsuarioAutenticado = () => {
+    return true;
+}
+
+function RotaAutenticada( { component: Component, ...props } ){
+    return (
+        <Route {...props} render={ (componentProps) => {
+            if(isUsuarioAutenticado()){
+                return (
+                    <Component {...componentProps} />
+                )
+            }else{
+                return(
+                    <Redirect to={ {pathname : '/login', state : { from: componentProps.location } } } />
+                )
+            }
+        }}  />
+    )
+}
 
 function Rotas(){
     return (
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastroUsuario} />
-                <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
-                <Route path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
+                
+                <RotaAutenticada path="/home" component={Home} />
+                <RotaAutenticada path="/consulta-lancamentos" component={ConsultaLancamentos} />
+                <RotaAutenticada path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
             </Switch>
         </HashRouter>
     )
